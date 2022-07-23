5a28e408bc06a9b08c1544cc545c8498c69ea4bf
Nichanok Auevechanichkul <nichanok.a@gmail.com>
2022-07-23 14:04:48 +0200

    Initial Push

---
+++ na_try_project/lib.py
@@ -0,0 +1,7 @@
+def try_me(name):
+    print(f"Dont worry {name}, be happy and keep smilling!")
+
+if __name__ == "__main__":
+    # Le Wagon location
+    say_yes = try_me("Max Musterman")
+    print(say_yes)
